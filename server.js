dde29b86f08fad6b43c885b272e99414ee47ad63
KSRL05 <104593511+KSRL05@users.noreply.github.com>
2024-11-17 11:54:17 -0500

    server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const http = require('http');
+Const http = require('http');
 Const port = 3000;
 
-const server = http.createserver((req, res => {
+Const server = http.createserver((req, res => {
 res.StatusCode = 200;
 res.setHeader('Content-type' , 'tet/plain');
 res.end('Hola Mundo');
